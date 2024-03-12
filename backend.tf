0eacf13b6fc28d621a6eaaae7ed0b55286ae37f2
Marcel E. <echomike@gmx.de>
2024-03-12 16:45:07 +0100

    fix backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "example-dev-20231208-tfstate"
     encrypt        = true
     key            = "terraform.tfstate"
     region         = "eu-central-1"
